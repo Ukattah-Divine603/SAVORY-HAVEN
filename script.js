bc107049f158ecfbb528f8f8fa652d3124ca074f
divi <ukattahdivine603@gmail.com>
2025-03-19 14:11:17 +0100

    Just Started Responsiveness

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const offScreenMenu = document.getElementById("menu-mobile");
+//  Tracks the Icon
+const hamMenu = document.getElementById("ham-menu");
+// The Ham Menu tracking the icon
+hamMenu.addEventListener("click", function () {
+  //
+  offScreenMenu.classList.toggle("open-menu");
+});
 
-
-
+function removeMenu() {
+  offScreenMenu.classList.remove("open-menu");
+}
